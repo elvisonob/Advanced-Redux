bd260b31d897689789662f307316711ac35001fc
elvisonob <elvisonob@yahoo.com>
2024-10-04 08:45:28 +0100

    solo practice

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
 import classes from './CartButton.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { shoppingAction } from './../store/index';
 
 const CartButton = (props) => {
+  const dispatchFn = useDispatch();
+
+  const onToggleCart = () => {
+    dispatchFn(shoppingAction.toggleCart());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={onToggleCart}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
---
+++ src/components/store/index.js
@@ -0,0 +1,20 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+const initialState = { cart: true };
+const shoppingOperations = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    toggleCart(state) {
+      state.cart = !state.cart;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: shoppingOperations.reducer,
+});
+
+export const shoppingAction = shoppingOperations.actions;
+
+export default store;
