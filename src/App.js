aa7ded99434568ac7619d76a34b83f1cf9136fbe
elvisonob <elvisonob@yahoo.com>
2024-10-31 04:58:40 +0000

    adding notifications

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
   const cartIsVisible = useSelector((state) => state.shoppingReducer.cart);
+  const cartContent = useSelector((state) => state.shoppingReducer.cartContent);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      const response = await fetch(
+        'https://redux-server-dce7e-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cartContent),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+      const responseData = await response.json();
+    };
+  }, [cartContent]);
   return (
     <Layout>
       {cartIsVisible && <Cart />}
